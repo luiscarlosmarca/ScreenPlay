c19d2e0658bd30d7f0009435292b9dd7a865a5d9
luis carlos marin <luiscarlosmarca@gmail.com>
2018-09-07 15:10:52 -0500

    config begin metodo

--- reto 1/src/test/java/co/com/proyectobase/screenplay/stepdefinitions/RegisterUserStepDefinition.java
+++ reto 1/src/test/java/co/com/proyectobase/screenplay/stepdefinitions/RegisterUserStepDefinition.java
@@ -3,17 +3,22 @@ package co.com.proyectobase.screenplay.stepdefinitions;
 import org.openqa.selenium.WebDriver;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.thucydides.core.annotations.Managed;
 
 public class RegisterUserStepDefinition {
 	@Managed (driver="chrome")
 	private WebDriver hisBrowser;
 	private Actor carlos = Actor.named("carlos");
-	
+	@Before
+	public void connfigBegin() {
+		carlos.can(BrowseTheWeb.with(hisBrowser));
+	}
 	@Given("^that Carlos want into to the web Automation Demo Site$")
 	public void thatCarlosWantIntoToTheWebAutomationDemoSite() throws Exception {
 	    // Write code here that turns the phrase above into concrete actions
